1f2405afdd8a4496e7f84b8f53e18de2edff1d0b
PerryVinner <43051105+PerryVinner@users.noreply.github.com>
2018-12-06 01:29:16 -0500

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,11 @@
+//A class that contains the questions and answers for problems passed onto by the Quiz class
+public class Question {
+    String prompt;
+    String answer;
+
+    public Question(String prompt, String answer) {
+        this.prompt = prompt;
+        this.answer = answer;
+
+    }
+}
